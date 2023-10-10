98790e3afc24fd74c0d8531abddefc218f025c03
sacpatil1995 <sachin@rtosoftware.com.au>
2023-10-10 11:36:39 +0100

    added some images and skill section

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent implements OnInit {
+  skills = [
+    { id: 1, name: 'C#' },
+    { id: 2, name: 'MVC' },
+    { id: 3, name: '.Net Core' },
+    { id: 4, name: 'Angular' },
+    { id: 5, name: 'React' },
+    { id: 6, name: 'JavaScript' },
+    { id: 7, name: 'HTML' },
+    { id: 8, name: 'CSS' },
+    { id: 9, name: 'SQL' },
+    { id: 10, name: 'Entity Framework' }
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    // Add more skills as needed
+  ];
+  constructor() {}
 
+  ngOnInit(): void {}
 }
